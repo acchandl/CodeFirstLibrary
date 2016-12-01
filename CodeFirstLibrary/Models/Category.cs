2f1fa24212d43aa4597e9bcd6aa31c711e4850e8
Alexis Chandler <alexiscchandler@gmail.com>
2016-12-01 07:43:12 -0500

    first commit

---
+++ CodeFirstLibrary/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CodeFirstLibrary.Models
+{
+    public class Category
+    {
+
+
+        public int Id { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
---
+++ CodeFirstLibrary/Models/CodeFirstLibraryContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstLibrary.Models
+{
+    public class CodeFirstLibraryContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public CodeFirstLibraryContext() : base("name=CodeFirstLibraryContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<CodeFirstLibrary.Models.Category> Categories { get; set; }
+
+        public System.Data.Entity.DbSet<CodeFirstLibrary.Models.Book> Books { get; set; }
+    }
+}
